9931a7a47d24193b849d9378aa2aac80a6fb7f6d
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-07-03 17:35:18 +0530

    feat: set framesize callback

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,41 @@
 #include <cstdlib>
 #include <ctime>
 
+const unsigned int SCR_WIDTH{800};
+const unsigned int SCR_HEIGHT{800};
+
+void framebuffer_size_callback(GLFWwindow *window, GLint width, GLint height);
+
+void process_input(GLFWwindow *window);
+
 int main()
 {
-    std::println("hello son");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window{glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OPENGLine",NULL,NULL)};
+    if (window == NULL) {
+        std::println("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     return 0;
 }
+
+void framebuffer_size_callback(GLFWwindow *window, GLint width, GLint height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window)
+{
+    if (glfwGetKey(window,GLFW_KEY_J) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
