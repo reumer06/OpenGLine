32af65d531fe3f163d041699db35ebaa039046ce
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-07-03 17:39:45 +0530

    feat: window setup

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,17 @@ int main()
         std::println("Failed to initialize GLAD");
         return -1;
     }
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
 
